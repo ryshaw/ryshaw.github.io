ce03761b3b01b0818de1aa28946956169e4aa1cd
Ryan Shaw <rshaw727@gmail.com>
2023-07-02 17:25:52 -0700

    bouncing balls 1.0

--- ball-game/main.js
+++ ball-game/main.js
@@ -8,7 +8,7 @@ const height = (canvas.height = window.innerHeight);
 
 // get score paragraph
 
-const ballCount = document.querySelector("p");
+const ballCount = document.querySelector(".ballCount");
 let score = 0;
 
 // function to generate random number
@@ -20,8 +20,22 @@ function random(min, max) {
 
 // function to generate random color
 
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+let colorTheme = "default";
+const beachColors = ["#526A9B", "#75A0C9", "#8BBFD3", "#C1D3E0", "#F3E9E0"];
+const forestColors = ["#3a5a40", "#656d4a", "#a4ac86", "#333d29", "#588157"];
+const celestialColors = ["#ffadad", "#9bf6ff", "#bdb2ff", "#ffc6ff", "#3a86ff"];
+
+function randomColor() {
+  switch (colorTheme) {
+    case "beach":
+      return beachColors[random(0, beachColors.length - 1)];
+    case "forest":
+      return forestColors[random(0, forestColors.length - 1)];
+    case "celestial":
+      return celestialColors[random(0, celestialColors.length - 1)];
+    default:
+      return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+  }
 }
 
 // parent class for Ball and player
@@ -79,7 +93,7 @@ class Ball extends Shape {
         const distance = Math.sqrt(dx * dx + dy * dy);
 
         if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
+          ball.color = this.color = randomColor();
         }
       }
     }
@@ -91,6 +105,7 @@ class EvilCircle extends Shape {
     super(x, y, 20, 20);
     this.color = "white";
     this.size = 10;
+    this.exist = false;
     window.addEventListener("keydown", (e) => {
       switch (e.key) {
         case "a":
@@ -153,7 +168,7 @@ class EvilCircle extends Shape {
 
 const balls = [];
 
-while (balls.length < 25) {
+while (balls.length < 30) {
   const size = random(10, 20);
   const ball = new Ball(
     // ball position always drawn at least one ball width
@@ -162,7 +177,7 @@ while (balls.length < 25) {
     random(0 + size, height - size),
     random(-7, 7),
     random(-7, 7),
-    randomRGB(),
+    randomColor(),
     size
   );
 
@@ -172,8 +187,62 @@ while (balls.length < 25) {
 
 const player = new EvilCircle(random(10, width - 10), random(10, height - 10));
 
+// setup start panel
+
+const startPanel = document.querySelector(".start");
+const startButton = document.querySelector(".startButton");
+
+startButton.addEventListener("click", () => {
+  startPanel.parentNode.removeChild(startPanel);
+  player.exist = true;
+});
+
+// setup themes
+const defaultButton = document.querySelector("#default");
+const beachButton = document.querySelector("#beach");
+const forestButton = document.querySelector("#forest");
+const celestialButton = document.querySelector("#celestial");
+
+let backgroundColor = "rgba(0, 0, 0, 0.25)";
+
+defaultButton.addEventListener("click", () => {
+  backgroundColor = "rgba(0, 0, 0, 0.25)";
+  colorTheme = "default";
+  for (const ball of balls) {
+    ball.color = randomColor();
+  }
+  player.color = "white";
+});
+
+beachButton.addEventListener("click", () => {
+  backgroundColor = "#E1D2CB";
+  colorTheme = "beach";
+  for (const ball of balls) {
+    ball.color = randomColor();
+  }
+  player.color = "#985952";
+});
+
+forestButton.addEventListener("click", () => {
+  backgroundColor = "#b6ad90";
+  colorTheme = "forest";
+  for (const ball of balls) {
+    ball.color = randomColor();
+  }
+  player.color = "#5dd39e";
+});
+
+celestialButton.addEventListener("click", () => {
+  backgroundColor = "#a09ebb";
+  colorTheme = "celestial";
+  for (const ball of balls) {
+    ball.color = randomColor();
+  }
+  player.color = "#03ddcf";
+});
+
 function loop() {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillStyle = backgroundColor;
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
@@ -184,9 +253,11 @@ function loop() {
     }
   }
 
-  player.draw();
-  player.checkBounds();
-  player.collisionDetect();
+  if (player.exist) {
+    player.draw();
+    player.checkBounds();
+    player.collisionDetect();
+  }
 
   ballCount.textContent = `Ball count: ${score}`;
 
