86f6acbd20a73f8703e07195db78cb13bc9ee7e0
Ryan Shaw <rshaw727@gmail.com>
2023-07-22 14:02:43 -0700

    update

--- games/midnight-ride/game.js
+++ games/midnight-ride/game.js
@@ -2,7 +2,7 @@ class MidnightRide extends Phaser.Scene {
   width; // width of game
   height; // height of game
   player;
-  cursors;
+  arrowKeys;
 
   constructor(config) {
     super(config);
@@ -15,9 +15,11 @@ class MidnightRide extends Phaser.Scene {
       "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"
     );
 
-    // load the tilemap
-    this.load.image("tiles", "./assets/tiled/test-tileset.png");
-    this.load.tilemapTiledJSON("testMap", "./assets/test.json");
+    // load the tilesets and the tilemap
+    this.load.image("landTileset", "./assets/tiled/tilesets/GB-LandTileset.png");
+    this.load.image("houseTileset", "./assets/tiled/tilesets/Universal-Buildings-and-walls.png");
+    this.load.image("roadTileset", "./assets/tiled/tilesets/Universal-Road-Tileset.png");
+    this.load.tilemapTiledJSON("map", "./assets/tiled/map.json");
 
     this.load.image("player", "./assets/player.png");
 
@@ -35,52 +37,71 @@ class MidnightRide extends Phaser.Scene {
       },
     });
 
-    const map = this.make.tilemap({ key: "testMap" });
-    const tileset = map.addTilesetImage("test", "tiles");
-    const groundLayer = map.createLayer("Ground", tileset).setScale(2);
-    groundLayer.setPosition(this.width / 2 - groundLayer.width, this.height / 2 - groundLayer.height);
-    const houseLayer = map.createLayer("House", tileset).setScale(2);
-    houseLayer.setPosition(this.width / 2 - houseLayer.width, this.height / 2 - houseLayer.height);
-  
-    groundLayer.setCollisionByProperty({ collision: true });
+    // create tilemap
+    const map = this.make.tilemap({ key: "map" });
+    const groundLayer = map.createLayer("Ground", map.addTilesetImage("land", "landTileset")).setScale(4);
+    groundLayer.setPosition(this.width / 2 - groundLayer.width * 2, this.height / 2 - groundLayer.height * 2);
 
-    this.player = this.physics.add.sprite(this.width / 2, this.height / 2, "player");
-    this.player.setScale(2);
+    const roadLayer = map.createLayer("Road", map.addTilesetImage("roads", "roadTileset")).setScale(4);
+    roadLayer.setPosition(this.width / 2 - roadLayer.width * 2, this.height / 2 - roadLayer.height * 2);
+
+    const houseLayer = map.createLayer("Houses", map.addTilesetImage("buildings", "houseTileset")).setScale(4);
+    houseLayer.setPosition(this.width / 2 - houseLayer.width * 2, this.height / 2 - houseLayer.height * 2);
+
+    const decorLayer = map.createLayer("Decorations", map.addTilesetImage("land", "landTileset")).setScale(4);
+    decorLayer.setPosition(this.width / 2 - decorLayer.width * 2, this.height / 2 - decorLayer.height * 2);
+  
+    // this next line sets up collision: if there is a road tile built on top of a ground tile,
+    // set collision to false so the player can walk on it. otherwise, it's just grass, 
+    // so set collision to true. the player can only walk on road tiles
+    groundLayer.forEachTile((tile) => {
+      roadLayer.hasTileAt(tile.x, tile.y) ? tile.setCollision(false) : tile.setCollision(true);
+    });
+
+    this.player = this.physics.add.sprite(this.width / 2, this.height / 2 - 32, "player");
+    this.player.setScale(4);
     this.player.body.setSize(8, 8);
+    this.player.body.isCircle = true;
 
     this.physics.add.collider(this.player, groundLayer);
+
     /*const debugGraphics = this.add.graphics().setAlpha(0.75);
     groundLayer.renderDebug(debugGraphics, {
-      tileColor: null,
+      tileColor: new Phaser.Display.Color(25, 25, 255, 50),
       collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
       faceColor: new Phaser.Display.Color(40, 39, 37, 255),
     })*/
 
+    this.arrowKeys = this.input.keyboard.createCursorKeys();
+    this.wasdKeys = this.input.keyboard.addKeys(
+      {'up': Phaser.Input.Keyboard.KeyCodes.W, 
+    'down': Phaser.Input.Keyboard.KeyCodes.S, 
+    'left': Phaser.Input.Keyboard.KeyCodes.A, 
+    'right': Phaser.Input.Keyboard.KeyCodes.D
+    });
 
-    this.cursors = this.input.keyboard.createCursorKeys();
+    this.cameras.main.startFollow(this.player, true, 0.2, 0.2);
+    this.physics.world.fixedStep = false;
   }
 
+
   update() {
-    this.player.body.setVelocity(0);
-
-    if (this.cursors.left.isDown) {
-      this.player.body.setVelocityX(-100);
+    if (this.arrowKeys.left.isDown || this.wasdKeys.left.isDown) {
+      this.player.body.setVelocity(-250, 0);
       this.player.setRotation(Math.PI * 3/2);
-    } else if (this.cursors.right.isDown) {
-      this.player.body.setVelocityX(100);
+      
+    } else if (this.arrowKeys.right.isDown || this.wasdKeys.right.isDown) {
+      this.player.body.setVelocity(250, 0);
       this.player.setRotation(Math.PI * 1/2);
-    }
 
-    if (this.cursors.up.isDown) {
-      this.player.body.setVelocityY(-100);
+    } else if (this.arrowKeys.up.isDown || this.wasdKeys.up.isDown) {
+      this.player.body.setVelocity(0, -250);
       this.player.setRotation(0);
-    } else if (this.cursors.down.isDown) {
-      this.player.body.setVelocityY(100);
+
+    } else if (this.arrowKeys.down.isDown || this.wasdKeys.down.isDown) {
+      this.player.body.setVelocity(0, 250);
       this.player.setRotation(Math.PI);
     }
-
-    this.player.body.velocity.normalize().scale(100);
-    //this.player.body.updateFromGameObject();
   }
 }
 
@@ -91,7 +112,7 @@ const config = {
   physics: {
     default: "arcade",
     arcade: {
-      debug: true,
+      debug: false,
     },
   },
   scaleMode: Phaser.Scale.FIT,
