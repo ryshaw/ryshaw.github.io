364fb77e766d799e39d7219a58c7e29a4aaa7491
Ryan Shaw <rshaw727@gmail.com>
2023-08-18 14:11:08 -0700

    up

--- games/wine-dark-sea/game.js
+++ games/wine-dark-sea/game.js
@@ -14,6 +14,9 @@ class Game extends Phaser.Scene {
   pointerLine; // line that follows p
   spears;
   fishes;
+  canThrowSpear;
+  mouseDown;
+  maxFish;
 
   constructor() {
     super({ key: "Game" });
@@ -28,6 +31,7 @@ class Game extends Phaser.Scene {
     this.load.image("ship", "assets/ship.png");
     this.load.image("spear", "assets/spear.png");
     this.load.image("fish", "assets/fish.png");
+    this.load.image("fish-dark", "assets/fish-dark.png");
     this.load.spritesheet("sun", "assets/sun.png", {
       frameWidth: 32,
       frameHeight: 32,
@@ -42,6 +46,9 @@ class Game extends Phaser.Scene {
     this.height = game.config.height;
     this.timer = 0;
     this.daytime = true;
+    this.canThrowSpear = true;
+    this.mouseDown = false;
+    this.maxFish = 20;
   }
 
   create() {
@@ -49,19 +56,14 @@ class Game extends Phaser.Scene {
 
     this.createFish();
 
-    // add UI stuff at the very end so it's above everything
-    this.UICamera = this.cameras.add(
-      this.cameras.main.x,
-      this.cameras.main.y,
-      this.cameras.main.width,
-      this.cameras.main.height
-    );
-
-    this.children.getAll().forEach((object) => {
-      this.UICamera.ignore(object);
-    });
+    this.createPhysics();
 
     this.addKeyboardControls();
+
+    // starts the fish respawn cycle
+    // every so often (~1 second), check if less than max fish
+    // if so, add a new fish, and repeat the cycle
+    this.time.delayedCall(1000, () => this.checkFish());
 
     WebFont.load({
       google: {
@@ -149,7 +151,7 @@ class Game extends Phaser.Scene {
 
     // from sunrise to sunset
     setInterval(() => {
-      let change = Math.PI * 0.01;
+      let change = Math.PI * 0.25;
       Phaser.Actions.RotateAroundDistance(
         [this.sun, this.moon],
         { x: circle.x, y: circle.y },
@@ -172,6 +174,16 @@ class Game extends Phaser.Scene {
       this.sun.setVisible(false);
       this.moon.setVisible(true);
       this.pointerLine.setFillStyle(0x000000, 1);
+
+      // I do not know why it won't remove all fishes in one go
+      // but apparently it takes multiple loops to remove all fish
+      let iterations = 10;
+      do {
+        iterations--;
+        this.fishes
+          .getChildren()
+          .forEach((fish) => this.fishes.remove(fish, true, true));
+      } while (this.fishes.getLength() > 0 && iterations > 0);
     } else {
       // switch to day
       this.sky.setFillStyle(0xffffff, 1);
@@ -193,15 +205,14 @@ class Game extends Phaser.Scene {
     );
     this.fishes = this.physics.add.group({
       key: "fish",
-      quantity: 10,
-      setScale: 2,
+      quantity: this.maxFish,
     });
     Phaser.Actions.RandomRectangle(this.fishes.getChildren(), bounds);
 
     this.fishes.getChildren().forEach((fish) => {
       this.physics.add.existing(fish);
       fish.body.setSize(12, 10);
-      let v = Phaser.Math.Between(20, 50);
+      let v = this.getFishVelocity();
       if (Math.random() > 0.5) {
         v *= -1;
         fish.setFlipX(true);
@@ -210,49 +221,103 @@ class Game extends Phaser.Scene {
       fish.body.setCollideWorldBounds(true);
       fish.body.onWorldBounds = true;
       fish.setName("fish");
-    });
 
+      this.tweens.chain({
+        targets: fish.body.velocity,
+        tweens: [
+          {
+            y: Phaser.Math.Between(2, 15),
+            duration: Phaser.Math.Between(600, 1800),
+          },
+          {
+            y: Phaser.Math.Between(-2, -15),
+            duration: Phaser.Math.Between(600, 1800),
+          },
+        ],
+        loop: -1,
+      });
+    });
+  }
+
+  createPhysics() {
     this.spears = this.physics.add.group();
 
-    this.physics.add.collider(this.spears, this.fishes, (obj1, obj2) => {
+    this.physics.add.overlap(this.spears, this.fishes, (obj1, obj2) => {
       this.hitFish(obj1, obj2);
     });
 
     this.physics.world.on("worldbounds", (body) => {
-      body.gameObject.destroy();
+      switch (body.gameObject.name) {
+        case "fish":
+          this.fishes.remove(body.gameObject, true, true);
+          break;
+        case "spear":
+          this.spears.remove(body.gameObject, true, true);
+          break;
+      }
     });
 
-    this.physics.world.setBounds(-16, 0, this.width + 32, this.height);
+    this.physics.world.setBounds(-32, 0, this.width + 64, this.height + 16);
+    //this.physics.world.setBounds(0, 0, this.width, this.height + 16);
+
+    this.physics.add.existing(this.sky, true);
+    this.physics.add.collider(this.sky, this.fishes);
   }
 
   hitFish(spear, fish) {
+    const t = new CustomText(this, fish.x + 8, fish.y, "+1", "l");
+    this.tweens.add({
+      targets: t,
+      y: t.y - 10,
+      duration: 300,
+      onComplete: () => t.destroy(),
+    });
+
     this.fishes.remove(fish, true, true);
-    this.spears.remove(spear, true, true);
-    console.log("hit");
   }
 
-  // function that goes off every second, for timers
-  timerTick() {}
+  checkFish() {
+    if (this.daytime && this.fishes.getLength() < this.maxFish) {
+      let x = -16;
+      if (Math.random() < 0.5) {
+        x = this.width + 16;
+      }
+      let y = Phaser.Math.Between(this.height / 2 + 32, this.height - 32);
+      const fish = this.physics.add.sprite(x, y, "fish");
+
+      fish.body.setSize(12, 10);
+      let v = this.getFishVelocity();
+      if (x == this.width + 16) {
+        v *= -1;
+        fish.setFlipX(true);
+      }
+      fish.setName("fish");
+      this.fishes.add(fish);
+      fish.body.setCollideWorldBounds(true);
+      fish.body.onWorldBounds = true;
+      fish.body.setVelocity(v, 0);
+      this.tweens.chain({
+        targets: fish.body.velocity,
+        tweens: [
+          {
+            y: Phaser.Math.Between(2, 15),
+            duration: Phaser.Math.Between(800, 1600),
+          },
+          {
+            y: Phaser.Math.Between(-2, -15),
+            duration: Phaser.Math.Between(800, 1600),
+          },
+        ],
+        loop: -1,
+      });
+    }
+    const t = Phaser.Math.Between(300, 800);
+    this.time.delayedCall(t, () => this.checkFish());
+  }
 
   addKeyboardControls() {
-    this.input.on("pointerdown", (p) => {
-      if (p.y >= this.height / 2) {
-        const spear = this.physics.add.sprite(
-          this.width / 2,
-          this.height / 2,
-          "spear"
-        );
-        this.spears.add(spear);
-
-        this.physics.moveTo(spear, p.x, p.y, 180);
-        spear.body.setSize(12);
-        spear.body.isCircle = true;
-        spear.rotation = spear.body.velocity.angle();
-        spear.body.setCollideWorldBounds(true);
-        spear.body.onWorldBounds = true;
-        spear.setName("spear");
-      }
-    });
+    this.input.on("pointerdown", (p) => (this.mouseDown = true));
+    this.input.on("pointerup", (p) => (this.mouseDown = false));
 
     /*
     this.input.keyboard.on("keydown-M", () => {
@@ -280,7 +345,45 @@ class Game extends Phaser.Scene {
     this.create();
   }
 
-  update() {}
+  getFishVelocity() {
+    return Phaser.Math.Between(20, 80);
+  }
+
+  update() {
+    const mouseX = this.input.activePointer.x;
+    const mouseY = this.input.activePointer.y;
+    if (this.mouseDown && mouseY >= this.height / 2 && this.canThrowSpear) {
+      if (!this.daytime) {
+      }
+      this.canThrowSpear = false;
+      const spear = this.physics.add.sprite(
+        this.width / 2,
+        this.height / 2,
+        "spear"
+      );
+      if (!this.daytime) {
+        spear.setTexture("fish-dark");
+      }
+
+      this.spears.add(spear);
+
+      this.physics.moveTo(spear, mouseX, mouseY, 180);
+      spear.body.setSize(12);
+      spear.body.isCircle = true;
+      spear.rotation = spear.body.velocity.angle();
+      spear.body.setCollideWorldBounds(true);
+      spear.body.onWorldBounds = true;
+      spear.setName("spear");
+
+      this.time.delayedCall(800, () => (this.canThrowSpear = true));
+    }
+
+    this.fishes.getChildren().forEach((fish) => {
+      if (fish.body.velocity.y == 0) {
+        //fish.body.velocity.y = 10;
+      }
+    });
+  }
 }
 
 const config = {
@@ -290,7 +393,7 @@ const config = {
   physics: {
     default: "arcade",
     arcade: {
-      debug: false,
+      debug: true,
     },
   },
   scale: {
@@ -318,23 +421,18 @@ class CustomText extends Phaser.GameObjects.Text {
       .text(x, y, text, {
         font:
           size == "g"
-            ? "48px"
+            ? "64px"
             : size == "l"
-            ? "30px"
+            ? "32px"
             : size == "m"
-            ? "18px"
-            : "12px",
+            ? "16px"
+            : "10px",
         fill: "#fff",
         align: "center",
-        lineSpacing: 16,
       })
-      .setDepth(2)
-      .setFontFamily('"GFS Didot"')
+      .setFontFamily('"Press Start 2P"')
       .setOrigin(align == "l" ? 0 : align == "c" ? 0.5 : 1, 0.5)
-      .setScrollFactor(0)
-      .setBackgroundColor("#1f1a1b")
-      .setPadding(10)
-      .setColor("#dad3d3");
+      .setScale(0.3);
 
     // if callback is given, assume it's a button and add callback
     if (callback) {
