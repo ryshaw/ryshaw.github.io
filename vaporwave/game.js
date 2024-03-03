c4d9964b48cd8218c7a73663bd947d0549a7e430
Ryan Shaw <rshaw727@gmail.com>
2024-03-03 11:42:51 -0800

    cleaning up

---
+++ vaporwave/game.js
@@ -0,0 +1,442 @@
+class Game extends Phaser.Scene {
+  // window resolution is 1280x720.
+  // game resolution is 320x180.
+  w;
+  h;
+  sounds;
+  keys;
+  graphics;
+
+  boundsGroup; // player and bounds collide, nothing else collides with bounds
+
+  constructor() {
+    super({ key: "Night" });
+  }
+
+  preload() {
+    this.load.script(
+      "webfont",
+      "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"
+    );
+
+    this.w = game.config.width;
+    this.h = game.config.height;
+  }
+
+  create() {
+    this.graphics = this.add.graphics({
+      lineStyle: { width: 0.2, color: 0xffd166 },
+      fillStyle: { color: 0xff00ff },
+    });
+
+    this.createLayout();
+    this.createControls();
+
+    WebFont.load({
+      google: {
+        families: [
+          "IBM Plex Mono",
+          "Finger Paint",
+          "Anonymous Pro",
+          "Roboto Mono",
+          "PT Sans",
+          "Quicksand",
+          "IBM Plex Sans",
+          "Titillium Web",
+        ],
+      },
+      active: () => {
+        this.loadGameUI();
+      },
+    });
+
+    console.log(this.w, this.h);
+
+    //this.cameras.main.fadeIn(800);
+    //this.UICamera.fadeIn(800);
+  }
+
+  createLayout() {
+    const top = "0x023e8a";
+    const bottom = "0x457b9d";
+    //this.graphics.fillGradientStyle(top, top, bottom, bottom);
+    //this.graphics.fillRect(0, 0, this.w, this.h);
+    const num1 = 0.1;
+    const num2 = 0.9;
+    this.add
+      .image(this.w / 2, this.h / 2, "__WHITE")
+      .setDisplaySize(this.w * 1, this.h * 1)
+      .preFX.addGradient(0x023e8a, 0x457b9d, 0.16, num1, num1, num2, num2, 18);
+  }
+
+  loadGameUI() {
+    new CustomText(
+      this,
+      this.w * 0.04,
+      this.h * 0.2,
+      "vaporwave tracks\nvol. 3",
+      "g",
+      "l"
+    )
+      .setFontSize("100px")
+      .postFX.addGlow(0xffffff, 0.45);
+
+    new CustomText(this, this.w * 0.9, this.h * 0.85, "enrique ramos", "g", "r")
+      .setFontSize("80px")
+      .postFX.addGlow(0xffffff, 0.45);
+  }
+
+  /*
+  createAudio() {
+    this.sounds = {
+      Day: this.sound.add("Day"),
+      Night: this.sound.add("Night"),
+      hit: this.sound.add("hit"),
+      lose: this.sound.add("lose"),
+      sfx: this.sound.add("sfx"),
+    };
+
+    this.sound.add("sea").play({
+      volume: 0.8,
+      loop: true,
+    });
+
+    this.sounds["Day"].play({
+      volume: 0.15,
+      loop: true,
+    });
+  }*/
+
+  createControls() {
+    this.input.on("pointerdown", (p) => (this.mouseDown = true));
+    this.input.on("pointerup", (p) => (this.mouseDown = false));
+
+    this.keys = this.input.keyboard.addKeys({
+      w: Phaser.Input.Keyboard.KeyCodes.W,
+      up: Phaser.Input.Keyboard.KeyCodes.UP,
+      s: Phaser.Input.Keyboard.KeyCodes.S,
+      down: Phaser.Input.Keyboard.KeyCodes.DOWN,
+      a: Phaser.Input.Keyboard.KeyCodes.A,
+      left: Phaser.Input.Keyboard.KeyCodes.LEFT,
+      d: Phaser.Input.Keyboard.KeyCodes.D,
+      right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
+    });
+
+    /*
+    this.input.keyboard.on("keydown-M", () => {
+      const track1 = this.sound.get("track1");
+      track1.isPlaying ? track1.pause() : track1.resume();
+    });
+
+    this.input.keyboard.on("keydown-N", () => {
+      this.soundVolume > 0 ? (this.soundVolume = 0) : (this.soundVolume = 0.8);
+      Object.values(this.soundEffects).forEach((sound) => sound.stop());
+    });*/
+  }
+
+  restartGame() {
+    this.children.getAll().forEach((object) => {
+      object.destroy();
+    });
+    this.input.removeAllListeners();
+    //this.input.keyboard.removeAllListeners();
+    this.tweens.killAll();
+    this.time.removeAllEvents();
+    this.sound.stopAll();
+    this.sound.removeAll();
+    this.anims.resumeAll();
+    this.matter.resume();
+    this.create();
+  }
+
+  update() {}
+
+  /*gameOver() {
+    /*
+    this.sound.stopAll();
+
+    this.sounds["lose"].play({
+      volume: 0.4,
+    });
+
+    this.matter.pause();
+    this.tweens.killAll();
+    this.anims.pauseAll();
+    this.isGameOver = true;
+    clearInterval(this.timeInterval);
+    clearInterval(this.waveInterval);
+
+    const t = new CustomText(
+      this,
+      this.windowW / 2,
+      this.windowH / 2,
+      `game over!\nyou lasted ${this.days} days\nclick to play again`,
+      "l",
+      "c"
+    )
+      .setColor("#9e2a2b")
+      .setPadding(15)
+      .setBackgroundColor("#f5ebe0")
+      .setLineSpacing(16)
+      .setDepth(2);
+
+    if (this.days == 1) {
+      t.setText(
+        `game over!\nyou lasted ${this.days} day...\nclick to play again`
+      );
+    }
+
+    this.time.delayedCall(500, () =>
+      this.input.once("pointerdown", () => this.restartGame())
+    );
+  }*/
+}
+
+class Start extends Phaser.Scene {
+  width;
+  height;
+
+  constructor() {
+    super({ key: "Start" });
+  }
+
+  preload() {
+    // load google's library for the font, GFS Didot
+    this.load.script(
+      "webfont",
+      "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"
+    );
+
+    this.load.audio("sea", "assets/audio/sea.mp3");
+
+    this.width = game.config.width;
+    this.height = game.config.height;
+  }
+
+  create() {
+    WebFont.load({
+      google: {
+        families: ["Press Start 2P"],
+      },
+      active: () => {
+        this.loadText();
+      },
+    });
+
+    const sea = this.sound.add("sea");
+    sea.play({
+      volume: 0.8,
+      loop: true,
+    });
+  }
+
+  loadText() {
+    this.add
+      .rectangle(0, 0, this.width, this.height / 2, 0xffffff)
+      .setOrigin(0, 0);
+    this.add
+      .rectangle(0, this.height / 2, this.width, this.height / 2, 0x000000)
+      .setOrigin(0, 0);
+
+    const t1 = new CustomText(
+      this,
+      this.width / 2,
+      this.height * 0.08,
+      "the fishy sea",
+      "g",
+      "c"
+    ).setFill("#000");
+
+    this.tweens.add({
+      targets: t1,
+      y: t1.y + 10,
+      yoyo: true,
+      duration: 1600,
+      loop: -1,
+      ease: "sine.inout",
+    });
+
+    const t2 = new CustomText(
+      this,
+      this.width / 2,
+      this.height * 0.25,
+      "how many nights can you last\nout on the wine-dark sea?",
+      "l",
+      "c"
+    ).setFill("#000");
+
+    const t3 = new CustomText(
+      this,
+      this.width / 2,
+      this.height * 0.42,
+      "during the day, use your mouse\nto send out hooks and collect fish",
+      "l",
+      "c"
+    ).setFill("#000");
+
+    const t4 = new CustomText(
+      this,
+      this.width / 2,
+      this.height * 0.62,
+      "during the night, use your mouse\nto send out your caught fish\nand protect yourself from lurking sharks",
+      "l",
+      "c"
+    ).setFill("#fff");
+
+    const t5 = new CustomText(
+      this,
+      this.width / 2,
+      this.height * 0.8,
+      "if too many sharks get to your ship...\nyou'll be sleeping with the fishes!",
+      "l",
+      "c"
+    ).setFill("#fff");
+
+    const t6 = new CustomText(
+      this,
+      this.width / 2,
+      this.height * 0.92,
+      "click me to start!",
+      "g",
+      "c",
+      () => {
+        this.sound.stopAll();
+        this.sound.removeAll();
+        this.scene.start("Game");
+      }
+    ).setFill("#fff");
+
+    this.tweens.add({
+      targets: t6,
+      y: t6.y + 10,
+      yoyo: true,
+      duration: 1600,
+      loop: -1,
+      ease: "sine.inout",
+    });
+  }
+}
+
+const config = {
+  type: Phaser.AUTO,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  physics: {
+    default: "arcade",
+    matter: {
+      debug: false,
+      gravity: {
+        x: 0,
+        y: 0,
+      },
+    },
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  pixelArt: true,
+  backgroundColor: "#000000",
+  scene: [Game],
+};
+
+class CustomText extends Phaser.GameObjects.Text {
+  constructor(
+    scene, // always "this" in the scene class
+    x,
+    y,
+    text,
+    size = "m", // s, m, or l for small, medium, or large
+    align = "c", // l, c, or r for left, center, or right
+    callback = null // provided only for buttons
+  ) {
+    super(scene);
+
+    this.clickedOn = false;
+
+    const cT = scene.add
+      .text(x, y, text, {
+        font:
+          size == "g"
+            ? "48px"
+            : size == "l"
+            ? "32px"
+            : size == "m"
+            ? "24px"
+            : "16px",
+        fill: "#fff",
+        align: "left",
+      })
+      .setFontFamily("Roboto Mono")
+      .setOrigin(align == "l" ? 0 : align == "c" ? 0.5 : 1, 0.5)
+      .setLineSpacing(16);
+
+    //"IBM Plex Mono", "Finger Paint", "Anonymous Pro"]
+    //"Roboto Mono", "PT Sans", "Quicksand", "IBM Plex Sans", "Titillium Web"
+
+    // if callback is given, assume it's a button and add callback.
+    // fine-tuned this code so button only clicks if player
+    // emits both pointerdown and pointerup events on it
+    if (callback) {
+      cT.setInteractive({ useHandCursor: true })
+        .setBackgroundColor("#3c6e71")
+        .setPadding(6)
+        .on("pointerover", function () {
+          this.setTint(0xeeeeee);
+        })
+        .on("pointerout", function () {
+          this.setTint(0xffffff).off("pointerup", callback, scene);
+        })
+        .on("pointerdown", function () {
+          this.setTint(0xdddddd);
+          if (this.listenerCount("pointerup") < 2) {
+            this.on("pointerup", callback, scene);
+          }
+        })
+        .on("pointerup", function () {
+          this.setTint(0xeeeeee);
+        });
+
+      // create dark green outline.
+      // i don't know how this works.
+      const bounds = cT.getBounds();
+      const rect = scene.add.rectangle(
+        bounds.x + bounds.width / 2,
+        bounds.y + bounds.height,
+        bounds.width + 6,
+        bounds.height + 6,
+        0x284b63,
+        1
+      );
+    }
+    return cT;
+  }
+}
+
+class CustomButton extends Phaser.GameObjects.Image {
+  constructor(
+    scene, // always "this" in the scene class
+    x,
+    y,
+    key,
+    callback = null // provided only for buttons
+  ) {
+    super(scene);
+
+    const cT = scene.add.image(x, y, key).setDepth(2);
+
+    // if callback is given, assume it's a button and add callback
+    if (callback) {
+      cT.setInteractive()
+        .on("pointerover", function () {
+          this.setTint(0xffffcc);
+        })
+        .on("pointerout", function () {
+          this.setTint(0xffffff);
+        })
+        .on("pointerdown", callback, scene);
+    }
+    return cT;
+  }
+}
+const game = new Phaser.Game(config);
